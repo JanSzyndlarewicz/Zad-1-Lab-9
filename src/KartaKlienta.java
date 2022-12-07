5205ade669c658996a8c5e9aedef2df81fc6c5a9
johnszyna <jan.szyndlarewicz@gmail.com>
2022-12-07 16:59:40 +0100

    KartaKlienta framework

---
+++ src/KartaKlienta.java
@@ -0,0 +1,40 @@
+public abstract class KartaKlienta {
+    private int numer;
+    private String nazwisko;
+
+    public KartaKlienta(){
+        numer = 0;
+        nazwisko = null;
+    }
+
+    public KartaKlienta(int numer, String nazwisko) {
+        this.numer = numer;
+        this.nazwisko = nazwisko;
+    }
+
+    public int getNumer() {
+        return numer;
+    }
+
+    public void setNumer(int numer) {
+        this.numer = numer;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public abstract int rabat();
+
+    @Override
+    public String toString() {
+        return "KartaKlienta{" +
+                "numer=" + numer +
+                ", nazwisko='" + nazwisko + '\'' +
+                '}';
+    }
+}
